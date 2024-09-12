b415edc92683c166b92871ef8c01dafc71fc7fec
matheusscode <matheussdev3@gmail.com>
2024-09-12 08:40:29 -0400

    feat: build a stack page

---
+++ app/[locale]/(app)/stack/page.tsx
@@ -0,0 +1,34 @@
+import { RadialPurpleBlurCircle } from "@/packages/components/background-radials";
+import { Separator } from "@/packages/components/ui/separator";
+import {
+  registry_tech_stack,
+  RegistryTechGroup,
+} from "@/registry/registry-tech-stack";
+import { Heading } from "@/ui/layout/heading";
+import { PageWrapper } from "@/ui/layout/page-wrapper";
+import { ContentWrapper } from "@/ui/layout/wrapper";
+import { TechGroupList } from "@/ui/stack/tech-group-list";
+
+export default function Page() {
+  return (
+    <PageWrapper>
+      <RadialPurpleBlurCircle />
+      <ContentWrapper className="flex flex-col gap-6">
+        <div>
+          <Heading title="Stack" />
+          <Separator />
+        </div>
+        {registry_tech_stack.map((group: RegistryTechGroup, index) => (
+          <nav
+            key={group.id}
+            data-index={index}
+            className="space-y-4 pt-8 data-[index=0]:pt-0"
+          >
+            <h1 className="mb-4 text-lg font-semibold">{group.groupName}</h1>
+            <TechGroupList group={group} />
+          </nav>
+        ))}
+      </ContentWrapper>
+    </PageWrapper>
+  );
+}
