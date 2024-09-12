fc2bac645b683030a1d397a1bd1c4a34ccb23e34
matheusscode <matheussdev3@gmail.com>
2024-09-12 08:40:14 -0400

    feat: build a workspaces page

---
+++ app/[locale]/(app)/workspaces/page.tsx
@@ -0,0 +1,30 @@
+import { RadialPurpleBlueFade } from "@/packages/components/background-radials";
+import { DefImage } from "@/packages/components/def-image";
+import { Separator } from "@/packages/components/ui/separator";
+import setup_print from "@/public/setup/setup-light.jpeg";
+import { Heading } from "@/ui/layout/heading";
+import { PageWrapper } from "@/ui/layout/page-wrapper";
+import { ContentWrapper } from "@/ui/layout/wrapper";
+import { WorkspaceDetailsTable } from "@/ui/workspaces/workspace-details-table";
+
+export default function Page() {
+  return (
+    <PageWrapper>
+      <RadialPurpleBlueFade />
+      <ContentWrapper className="flex flex-col gap-6">
+        <div>
+          <Heading title="Workspace" className="flex flex-col gap-3" />
+          <Separator />
+        </div>
+        <DefImage
+          src={setup_print}
+          alt="Light-themed setup illustration showing workspace layout"
+          width={540}
+          height={100}
+          className="my-4"
+        />
+        <WorkspaceDetailsTable />
+      </ContentWrapper>
+    </PageWrapper>
+  );
+}
