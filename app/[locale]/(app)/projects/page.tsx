f59665c8788de6d67923fb883f92eb710afdd909
matheusscode <matheussdev3@gmail.com>
2024-09-16 08:56:22 -0400

    feat: introduce suspense api in projects page

--- app/[locale]/(app)/projects/page.tsx
+++ app/[locale]/(app)/projects/page.tsx
@@ -1,5 +1,6 @@
 import { getProfile, getRepos } from "@/app/_services/github";
 import { GithubProfile, GithubRepository } from "@/interfaces/github";
+import { Spinner } from "@/packages/components/spinner";
 import { Separator } from "@/packages/components/ui/separator";
 import { ContentWrapper } from "@/ui/layout/content-wrapper";
 import { Heading } from "@/ui/layout/heading";
@@ -9,6 +10,7 @@ import { GithubGistJSON } from "@/ui/projects/github-gist-json";
 import { GithubProfileDetails } from "@/ui/projects/github-profile-details";
 import { GithubRepositoryCard } from "@/ui/projects/github-repository-card";
 import { getTranslations, unstable_setRequestLocale } from "next-intl/server";
+import { Suspense } from "react";
 
 interface Props {
   params: {
@@ -17,55 +19,57 @@ interface Props {
 }
 
 export default async function Page({ params: { locale } }: Props) {
-  unstable_setRequestLocale(locale);
   const t = await getTranslations({ locale, namespace: "projects" });
+  unstable_setRequestLocale(locale);
 
   const user: GithubProfile = await getProfile();
-  const repository: GithubRepository[] = await getRepos();
+  const repositories: GithubRepository[] = await getRepos();
   const gist = await fetch(process.env.VSCODE_SETTINGS_GIST_URL!);
   const trasnformerGist = await gist.text();
 
   return (
     <PageWrapper>
       <Transmutation>
-        <ContentWrapper className="flex flex-col gap-6">
-          <div>
-            <Heading title={t("main_title")} />
-            <Separator />
-          </div>
-          <GithubProfileDetails user={user} />
-          <div className="mt-14 flex flex-col">
+        <Suspense fallback={<Spinner />}>
+          <ContentWrapper className="flex flex-col gap-6">
             <div>
-              <h1 className="text-lg">{t("repositories_title")}</h1>
-              <p className="mb-0 text-sm text-primary/80 dark:text-muted-foreground/90">
-                {t("repositories_description")}
-              </p>
+              <Heading title={t("main_title")} />
+              <Separator />
             </div>
-            <div className="mt-3.5 grid grid-cols-1 gap-2 laptop:grid-cols-2">
-              {repository
-                .map((repo) => (
-                  <GithubRepositoryCard key={repo?.id} repo={repo} />
-                ))
-                .reverse()}
+            <GithubProfileDetails user={user} />
+            <div className="mt-14 flex flex-col">
+              <div>
+                <h1 className="text-lg">{t("repositories_title")}</h1>
+                <p className="mb-0 text-sm text-primary/80 dark:text-muted-foreground/90">
+                  {t("repositories_description")}
+                </p>
+              </div>
+              <div className="mt-3.5 grid grid-cols-1 gap-2 laptop:grid-cols-2">
+                {repositories
+                  .map((repo) => (
+                    <GithubRepositoryCard key={repo?.id} repo={repo} />
+                  ))
+                  .reverse()}
+              </div>
+              <span className="mt-4 text-center text-xs text-muted-foreground">
+                {t("repositories_caption")}
+              </span>
             </div>
-            <span className="mt-4 text-center text-xs text-muted-foreground">
-              My repositories ❤️
-            </span>
-          </div>
-          <Separator className="mb-3.5 mt-2" />
-          <div className="flex flex-col">
-            <div>
-              <h1 className="text-lg">Gist</h1>
-              <p className="mb-0 text-sm text-primary/80 dark:text-muted-foreground/90">
-                {t("gist_description")}
-              </p>
+            <Separator className="mb-3.5 mt-2" />
+            <div className="flex flex-col">
+              <div>
+                <h1 className="text-lg">Gist</h1>
+                <p className="mb-0 text-sm text-primary/80 dark:text-muted-foreground/90">
+                  {t("gist_description")}
+                </p>
+              </div>
+              <GithubGistJSON schema={trasnformerGist} />{" "}
+              <span className="mt-4 text-center text-xs text-muted-foreground">
+                {t("gist_caption")}
+              </span>
             </div>
-            <GithubGistJSON schema={trasnformerGist} />{" "}
-            <span className="mt-4 text-center text-xs text-muted-foreground">
-              My Visual Studio Code Config ❤️
-            </span>
-          </div>
-        </ContentWrapper>
+          </ContentWrapper>
+        </Suspense>
       </Transmutation>
     </PageWrapper>
   );
